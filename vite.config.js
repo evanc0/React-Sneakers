bc002903058d47fe4d543a28290f5ee9efc6e55e
Ivan Gryanik <ivangryanik46@gmail.com>
2023-12-13 21:56:56 +0300

    pred reliz

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://evanc0.github.io/React-Sneakers"
+  base: "/React-Sneakers/"
 })
